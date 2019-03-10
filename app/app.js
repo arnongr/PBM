efb3517580b71ad7dc11a0eb9f8be80b60b00236
arnon <arnongratch@gmail.com>
2019-03-10 11:09:10 +0200

    LOGIN - added login.html and redirect from Login button on navbar

--- app/app.js
+++ app/app.js
@@ -4,15 +4,14 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/", {
         templateUrl: "app/landing/landing.html",
-        controller: "getStartedCtrl"
+        controller: "getStartedCtrl",
     }).when("/getStarted", {
         templateUrl: "app/getStarted/get-started.html",
+    }).when("/login", {
+        templateUrl: "app/login/login.html"
     }).when("/workSpace", {
         templateUrl: "app/work-space/work-space.html"
-    //     controller: "movieCtrl"
-    // }).when("/movies/:movieId", {
-    //     templateUrl: "Movie Details/movieDetails.html",
-    //     controller: "movieDetailsCtrl"
+
 
     }).otherwise({
         redirectTo: "/"
--- app/landing/getStartedCtrl.js
+++ app/landing/getStartedCtrl.js
@@ -1,9 +1,35 @@
 
-app.controller("getStartedCtrl", function($scope, $location, $log) {
+app.controller("getStartedCtrl", function ($scope, $location, $log, userSrv) {
 
     //Redirect to 'Get Started' page
     $scope.getStarted = function () {
         $location.path("/getStarted");
-      }
+    }
+
+    //Redirect to 'Work Space' page
+
+
+    //Redirect to 'Login' page
+    $scope.login = function () {
+        $location.path("/login");
+    }
+
+
+    //Login
+    $scope.invalidLogin = false;
+    $scope.email = "arnon.work@gmail.com";
+    $scope.pwd = "123";
+
+
+
+    // $scope.login = function () {
+
+    //     userSrv.login($scope.email, $scope.pwd).then(function (activeUser) {
+    //         $location.path("/login");
+    //     }, function () {
+    //         $scope.invalidLogin = true;
+    //     });
+
+    // }
 
 })
