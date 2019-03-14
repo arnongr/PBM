56f1dd524f44da3259d685397ec1d80cf135559f
arnon <arnongratch@gmail.com>
2019-03-14 10:37:29 +0200

    LOGIN - capability added with loginCtrl and userSrv

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,6 +1,12 @@
 
 app.controller("loginCtrl", function ($scope, $location, $log, userSrv) {
 
+    $scope.logIn = function() {
+
+        userSrv.logIn($scope.email, $scope.password).then(function(activeUser) {
+            $location.path("/projects");
+
+        })}
 
 
 })
--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -1,5 +1,34 @@
 app.factory("userSrv", function ($http, $q, $log) {
 
+    var activeUser = null;
+
+    function User(parseUser) {
+        this.email = parseUser.get("email");
+        this.password = parseUser.get("password");
+    }
+
+    function logIn(email, password) {
+        var async = $q.defer();
+
+
+        // Pass the username and password to logIn function
+        Parse.User.logIn(email, password).then(function(user) {
+            // Do stuff after successful login
+            $log.info('Logged in user', user);
+            activeUser = new User(user);
+            async.resolve(activeUser);
+        }).catch(function(error) {
+            $log.error('Error while logging in user', error);
+            async.reject(error);
+        });
+
+        return async.promise;
+    }
+
+
+    return  {
+        logIn: logIn
+    }
 
 
 });
