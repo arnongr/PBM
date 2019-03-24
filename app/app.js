91151b5c9430c714cc932e6c524baf0a3f6ee5ba
arnongra <arnongratch@gmail.com>
2019-03-24 21:28:06 +0200

    Istalled Angular Charts correctly

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("PBM", ["ngRoute"]);
+var app = angular.module("PBM", ["ngRoute", "chart.js"]);
 
 app.config(function ($routeProvider) {
 
--- app/model/itemsPieChartDir.js
+++ app/model/itemsPieChartDir.js
@@ -1,4 +1,10 @@
-angular.module("app", ["chart.js"]).controller("itemsPieChart", function ($scope) {
+// app.directive("PieCtrl", function () {
+  angular.module("app", ["chart.js"]).controller("PieCtrl", function ($scope) {
     $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
     $scope.data = [300, 500, 100];
+    return {
+      templateUrl: "app/charts/item-pie-chart.html",
+      restrict: "E"
+    }
   });
+
